c4e3e1913fac08ad12fd8238ca63a941ffbaea60
mashiro <y.mashiro@gmail.com>
2010-08-22 04:42:58 +0900

    added: more infomationso

--- linqy/__init__.py
+++ linqy/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-'''
-	Linqy - LINQ like library for Python
-'''
+''' Linqy - LINQ like library for Python '''
 
 __author__ = 'mashiro'
+__email__ = 'y.mashiro@gmail.com'
 __license__ = 'MIT'
 __version__ = '0.1'
-
+__url__ = 'http://github.com/mashiro/linqy'
+  
 from linqy.enumerable import *
 
