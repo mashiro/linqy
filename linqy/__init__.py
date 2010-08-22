57b6bf803811a4526ab1e0cecbcd7e3e584f3b48
mashiro <y.mashiro@gmail.com>
2010-08-22 20:49:45 +0900

    up to 0.2

--- linqy/__init__.py
+++ linqy/__init__.py
@@ -6,7 +6,7 @@
 __author__ = 'mashiro'
 __email__ = 'y.mashiro@gmail.com'
 __license__ = 'MIT'
-__version__ = '0.1'
+__version__ = '0.2'
 __url__ = 'http://github.com/mashiro/linqy'
   
 from linqy.enumerable import *
