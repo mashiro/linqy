af078f3765dbac64ea237cd25c00b0153fb823cb
mashiro <y.mashiro@gmail.com>
2010-09-01 16:14:52 +0900

    modified: import

--- linqy/__init__.py
+++ linqy/__init__.py
@@ -9,5 +9,16 @@ __license__ = 'MIT'
 __version__ = '0.2.3'
 __url__ = 'http://github.com/mashiro/linqy'
 
-from linqy.enumerable import *
+from linqy.enumerable import (
+		Enumerable, SequenceEnumerable, OrderedEnumerable,
+		make, empty, range, repeat, cycle, countup,
+		asenumerable, toarray, tolist,
+		select, selectmany, zip, enumerate,
+		where, oftype,
+		orderby, orderby_descending, thenby, thenby_descending, reverse,
+		skip, skipwhile, take, takewhile,
+		sequenceequal,
+		foreach, do)
+from linqy.function import Function
+from linqy.utils import anonymouse
 
